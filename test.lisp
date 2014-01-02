9b7f9686b25c397fce6f7982bdc1262e95d3b234
Alexander Brook Perry <alex@alexgeek.co.uk>
2014-01-02 15:29:30 +0000

    Better

--- test.lisp
+++ test.lisp
@@ -16,7 +16,7 @@
         ;; if we reach end of expression just return term to be appended to expression
         ((null (term expr)) t1)
         ;; if equal variable and power, add coefficients
-        ( (eq (var expr) (car (cadr t1))) (list (list (+ (coefficient expr) (car t1)) (cadr t1))))
+        ( (and (eq (var expr) (car (cadr t1))) (= (index expr) (cadr (cadr t1)))) (list (list (+ (coefficient expr) (car t1)) (cadr t1))))
         ;; recursive case on next term in expr
         (t (p+term (cdr expr) t1))))
 
@@ -33,3 +33,5 @@
 (princ (index '((2 (x 1)))))
 (print "P+t: ")
 (princ (p+term '((10 (x 1))) '(-5 (x 1)) ))
+(print "P+t: ")
+(princ (p+term '((10 (x 2))) '(-5 (x 1)) ))
